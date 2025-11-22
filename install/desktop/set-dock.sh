648e1cd7128c0cfd86c26e33de385ccdafce1a2d
lfsc09 <lfsc09@gmail.com>
2025-11-22 17:24:39 -0300

    feat: added dock to remove favorite apps

---
+++ install/desktop/set-dock.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# If want to set favorite apps check (https://github.com/basecamp/omakub/blob/master/install/desktop/set-dock.sh)
+
+# Set the favorite apps to empty
+gsettings set org.gnome.shell favorite-apps '[]'
