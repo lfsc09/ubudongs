0d299210e07f1048f5763fc6142f14bda9b1f007
lfsc09 <lfsc09@gmail.com>
2025-11-22 07:49:33 -0300

    fix: permissions to install scripts

--- install/terminal/additional/audio/install.sh
+++ install/terminal/additional/audio/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp ~/.local/share/ubudongs/install/terminal/additional/audio/cycle-output-devices.run.sh $UBUDONGS_ADDITIONAL_TOOLS_DIR/cycle-output-devices.run.sh
-chmod +x $UBUDONGS_ADDITIONAL_TOOLS_DIR/cycle-output-devices.run.sh
+sudo cp ~/.local/share/ubudongs/install/terminal/additional/audio/cycle-output-devices.run.sh $UBUDONGS_ADDITIONAL_TOOLS_DIR/cycle-output-devices.run.sh
+sudo chmod +x $UBUDONGS_ADDITIONAL_TOOLS_DIR/cycle-output-devices.run.sh
 
-cp ~/.local/share/ubudongs/install/terminal/additional/audio/cycle-output-devices.setup.sh $UBUDONGS_ADDITIONAL_TOOLS_DIR/cycle-output-devices.setup.sh
-chmod +x $UBUDONGS_ADDITIONAL_TOOLS_DIR/cycle-output-devices.setup.sh
+sudo cp ~/.local/share/ubudongs/install/terminal/additional/audio/cycle-output-devices.setup.sh $UBUDONGS_ADDITIONAL_TOOLS_DIR/cycle-output-devices.setup.sh
+sudo chmod +x $UBUDONGS_ADDITIONAL_TOOLS_DIR/cycle-output-devices.setup.sh
