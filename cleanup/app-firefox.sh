ae952ed0067d2cef4e2550161d18afa26f117258
lfsc09 <lfsc09@gmail.com>
2025-11-24 12:23:19 -0300

    fix: cleanup uninstalls

--- cleanup/app-firefox.sh
+++ cleanup/app-firefox.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo apt remove -y firefox
+# Try removing from apt first
+sudo apt-get remove --purge -y firefox
+
+# Then try removing from snap
+sudo snap remove firefox
--- cleanup/app-nano.sh
+++ cleanup/app-nano.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt remove -y nano
+sudo apt-get remove --purge -y nano
