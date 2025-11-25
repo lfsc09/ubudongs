a642908b9801f556dd6cb51c9f33ad5d1d1163e9
lfsc09 <lfsc09@gmail.com>
2025-11-25 02:40:07 -0300

    fix: icons src folder

--- install/desktop/applications.sh
+++ install/desktop/applications.sh
@@ -5,6 +5,6 @@ APP_DEST_DIR="$HOME/.local/share/applications"
 # Copy application icons
 ICONS_DEST_DIR="$APP_DEST_DIR/icons"
 if [ ! -d "$ICONS_DEST_DIR" ]; then mkdir -p "$ICONS_DEST_DIR"; fi
-cp ./icons/*.png "$ICONS_DEST_DIR/"
+cp ~/.local/share/ubudongs/applications/icons/*.png "$ICONS_DEST_DIR/"
 
 for script in ~/.local/share/ubudongs/applications/*.sh; do source $script; done
