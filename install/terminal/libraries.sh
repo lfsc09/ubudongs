89f6b6e0243a5a1794aa26b2f4115377b4f771d3
lfsc09 <lfsc09@gmail.com>
2025-11-25 02:36:24 -0300

    refactor: removed unecessary libraries

--- install/terminal/libraries.sh
+++ install/terminal/libraries.sh
@@ -5,5 +5,4 @@ sudo apt install -y \
   pulseaudio-utils libnotify-bin \
   build-essential pkg-config autoconf bison clang rustc pipx \
   libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
-  libvips ffmpeg imagemagick libmagickwand-dev mupdf mupdf-tools \
-  redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev libpq-dev postgresql-client postgresql-client-common
+  libvips ffmpeg imagemagick libmagickwand-dev mupdf mupdf-tools
