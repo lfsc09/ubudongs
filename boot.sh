5bbf88ab849c9b3a7fe894173e20b2bbc4f64fd4
lfsc09 <lfsc09@gmail.com>
2025-11-26 14:04:17 -0300

    refactor: separate ascii art

--- boot.sh
+++ boot.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e
 
-ascii_art='
-  _   _ _               _                        
- | | | | |__  _   _  __| | ___  _ __   __ _ ___ 
- | | | | '\''_ \| | | |/ _` |/ _ \| '\''_ \ / _` / __|
- | |_| | |_) | |_| | (_| | (_) | | | | (_| \__ \
-  \___/|_.__/ \__,_|\__,_|\___/|_| |_|\__, |___/
-                                      |___/     
-'
-
 # Define some colors for the rest of the installation
 OK='\033[0;32m' # Green
 INFO='\033[0;35m' # Purple
@@ -17,8 +8,9 @@ WARNING='\033[1;33m' # Yellow
 ERROR='\033[0;31m' # Red
 NC='\033[0m' # No Color
 export OK INFO WARNING ERROR NC
+export UBUDONGS_PATH="$HOME/.local/share/ubudongs"
 
-echo -e "$ascii_art"
+echo -e "${OK}$(cat ascii-art)${NC}"
 echo -e "${WARNING}=> Ubudongs is for fresh Ubuntu 24.04+ installations only!${NC}"
 echo -e "${INFO}\n[Info] Begin installation (or abort with ctrl+c)...${NC}"
 
@@ -26,9 +18,10 @@ sudo apt-get update >/dev/null
 sudo apt-get install -y git >/dev/null
 
 echo -e "${INFO}[Info] Cloning Ubudongs...${NC}"
-rm -rf ~/.local/share/ubudongs
-git clone https://github.com/lfsc09/ubudongs.git ~/.local/share/ubudongs >/dev/null
+rm -rf $UBUDONGS_PATH
+git clone https://github.com/lfsc09/ubudongs.git $UBUDONGS_PATH >/dev/null
 
-echo -e "${WARNING}=> $(cat ~/.local/share/ubudongs/version)${NC}"
+echo -e "${WARNING}=> $(cat $UBUDONGS_PATH/version)${NC}"
 echo -e "${INFO}[Info] Installation starting...${NC}"
-source ~/.local/share/ubudongs/install.sh
+
+source $UBUDONGS_PATH/install.sh
