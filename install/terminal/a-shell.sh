3b9b8f59c6ad850108240773179028de2e538064
lfsc09 <lfsc09@gmail.com>
2025-11-26 14:07:50 -0300

    refactor: simplification of bash and bashrc installation

--- install/terminal/a-shell.sh
+++ install/terminal/a-shell.sh
@@ -2,14 +2,23 @@
 
 # Configure the bash shell using Ubudongs defaults
 [ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.bak
-cp ~/.local/share/ubudongs/configs/bashrc ~/.bashrc
+cp $UBUDONGS_PATH/configs/bashrc ~/.bashrc
 
-# Load the PATH for use later in the installers
-source ~/.local/share/ubudongs/defaults/bash/shell
+# Configure the inputrc using Ubudongs defaults
+[ -f ~/.inputrc ] && mv ~/.inputrc ~/.inputrc.bak
+cp $UBUDONGS_PATH/configs/inputrc ~/.inputrc
 
-# Load gnome terminal profile configs
-default_profile=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \')
-echo "[:$default_profile]" > /tmp/gnome-terminal.default
-cat ~/.local/share/ubudongs/configs/gnome-terminal >> /tmp/gnome-terminal.default
+# Copy bash function definitions
+[ -f ~/.bash_functions ] && mv ~/.bash_functions ~/.bash_functions.bak
+cp $UBUDONGS_PATH/configs/bash_functions ~/.bash_functions
+
+# Copy bash aliases
+[ -f ~/.bash_aliases ] && mv ~/.bash_aliases ~/.bash_aliases.bak
+cp $UBUDONGS_PATH/configs/bash_aliases ~/.bash_aliases
+
+# Load gnome-terminal profile configs
+default_profile_id=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \')
+echo "[:$default_profile_id]" > /tmp/gnome-terminal.default
+cat $UBUDONGS_PATH/configs/gnome-terminal/config >> /tmp/gnome-terminal.default
 cat /tmp/gnome-terminal.default | dconf load /org/gnome/terminal/legacy/profiles:/
 rm /tmp/gnome-terminal.default
