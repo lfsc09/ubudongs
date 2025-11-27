437c3e3702e76525c7ac6e8999d9d99bd56fb14d
lfsc09 <lfsc09@gmail.com>
2025-11-26 22:17:18 -0300

    fix: removing duplicated libs

--- install/terminal/libraries.sh
+++ install/terminal/libraries.sh
@@ -4,5 +4,5 @@ sudo apt install -y \
   wget gpg gnupg gpg-agent uidmap \
   pulseaudio-utils libnotify-bin \
   build-essential pkg-config autoconf bison clang rustc pipx \
-  libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
+  libssl-dev libreadline-dev zlib1g-dev libyaml-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
   libvips ffmpeg imagemagick libmagickwand-dev mupdf mupdf-tools
