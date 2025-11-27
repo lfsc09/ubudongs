5347b9f1fc93cd1727ed5199447bc8a7d171625c
lfsc09 <lfsc09@gmail.com>
2025-11-27 09:38:56 -0300

    fix: project cleanup after reboot (#3)

--- cleanup/project.sh
+++ cleanup/project.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Wait 10 seconds after boot to ensure system is ready, then delete the Ubudongs directory
-(crontab -l 2>/dev/null; echo "@reboot sleep 10 && rm -rf \"$UBUDONGS_PATH\"") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot sleep 10 && rm -rf \"\$HOME/.local/share/ubudongs\"") | crontab -
