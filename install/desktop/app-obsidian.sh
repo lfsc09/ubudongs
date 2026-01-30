b59ceddbb209c03f4dd0da24ea30419feebeda95
lfsc09 <lfsc09@gmail.com>
2026-01-29 21:55:48 -0300

    feat: add JetBrains Mono font

--- install/desktop/app-obsidian.sh
+++ install/desktop/app-obsidian.sh
@@ -4,6 +4,8 @@ if ! command -v obsidian &> /dev/null; then
   print_header "Obsidian installation"
 
   cd /tmp
+
+  # Get the latest version tag of Obsidian from GitHub API
   OBSIDIAN_VERSION=$(curl -s https://api.github.com/repos/obsidianmd/obsidian-releases/releases/latest | grep -Po '"tag_name": "v\K[^"]*')
   wget -O obsidian.deb "https://github.com/obsidianmd/obsidian-releases/releases/latest/download/obsidian_${OBSIDIAN_VERSION}_amd64.deb"
   sudo apt install -y ./obsidian.deb
--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -10,9 +10,17 @@ unzip FiraCode.zip -d FiraCodeFont
 cp FiraCodeFont/*.ttf ~/.local/share/fonts
 rm -rf FiraCode.zip FiraCodeFont
 
+# Get the latest version tag of JetBrains Mono from GitHub API
+JETBRAINS_VERSION=$(curl -s https://api.github.com/repos/JetBrains/JetBrainsMono/releases/latest | grep -Po '"tag_name": "v\K[^"]*')
+wget https://github.com/JetBrains/JetBrainsMono/releases/download/v${JETBRAINS_VERSION}/JetBrainsMono-${JETBRAINS_VERSION}.zip
+unzip JetBrainsMono-${JETBRAINS_VERSION}.zip -d JetBrainsMonoFont
+cp JetBrainsMonoFont/*.ttf ~/.local/share/fonts
+rm -rf JetBrainsMono-${JETBRAINS_VERSION}.zip JetBrainsMonoFont
+
 fc-cache
 cd -
 
 log_skipline
 log_success "FiraCode fonts installed"
+log_success "JetBrains Mono fonts installed"
 print_footer
