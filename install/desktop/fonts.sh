eade36d9f0e789f9695379fbd51414a7ddc53a1e
lfsc09 <lfsc09@gmail.com>
2026-01-31 14:51:43 -0300

    fix: font files copy after unzip

--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-print_header "FiraCode fonts installation"
+print_header "Fonts installation"
 
 mkdir -p ~/.local/share/fonts
 
 cd /tmp
 wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip
 unzip FiraCode.zip -d FiraCodeFont
-cp FiraCodeFont/*.ttf ~/.local/share/fonts
+find FiraCodeFont -type f -name "*.ttf" -exec cp {} ~/.local/share/fonts \;
 rm -rf FiraCode.zip FiraCodeFont
 
 # Get the latest version tag of JetBrains Mono from GitHub API
 JETBRAINS_VERSION=$(curl -s https://api.github.com/repos/JetBrains/JetBrainsMono/releases/latest | grep -Po '"tag_name": "v\K[^"]*')
 wget https://github.com/JetBrains/JetBrainsMono/releases/download/v${JETBRAINS_VERSION}/JetBrainsMono-${JETBRAINS_VERSION}.zip
 unzip JetBrainsMono-${JETBRAINS_VERSION}.zip -d JetBrainsMonoFont
-cp JetBrainsMonoFont/*.ttf ~/.local/share/fonts
+find JetBrainsMonoFont -type f -name "*.ttf" -exec cp {} ~/.local/share/fonts \;
 rm -rf JetBrainsMono-${JETBRAINS_VERSION}.zip JetBrainsMonoFont
 
 fc-cache
