7fd0a4cdcb569d4a0b8f6b1ef9442913c5c63d71
lfsc09 <lfsc09@gmail.com>
2026-02-02 12:48:38 -0300

    fix: gimp installation

--- install/desktop/app-gimp.sh
+++ install/desktop/app-gimp.sh
@@ -3,7 +3,7 @@
 if ! command -v gimp &> /dev/null; then
   print_header "Gimp installation"
   
-  flatpak install https://flathub.org/repo/appstream/org.gimp.GIMP.flatpakref
+  sudo apt install -y gimp
 
   log_skipline
   log_success "Gimp installed"
