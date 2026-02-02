dcb7e77b6fcf86af9ba054335adf1517277f1283
lfsc09 <lfsc09@gmail.com>
2026-02-02 12:43:47 -0300

    fix: optional apps selection

--- install/select-optional-apps.sh
+++ install/select-optional-apps.sh
@@ -3,6 +3,6 @@
 # Only ask for default desktop app choices when running Gnome
 if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
   optional_apps=("JetBrains Toolbox" "Obs Studio" "Solaar" "Zoom")
-  default_optional_apps='JetBrains Toolbox,Solaar'
+  default_optional_apps='Solaar'
   export UBUDONGS_SELECTED_OPTIONAL_APPS=$(gum choose "${optional_apps[@]}" --no-limit --selected $default_optional_apps --height 7 --header "Select optional apps" | tr ' ' '-')
 fi
