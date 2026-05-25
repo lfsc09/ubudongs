da90878fd3dcfeca6569ad0a06692800f59091e4
lfsc09 <lfsc09@gmail.com>
2026-05-25 08:37:55 -0300

    fix: lazydocker installation

--- install/terminal/lazydocker.sh
+++ install/terminal/lazydocker.sh
@@ -3,13 +3,7 @@
 if ! command -v lazydocker &> /dev/null; then
   print_header "Lazydocker installation"
 
-  cd /tmp
-  lazydocker_version=$(curl -s "https://api.github.com/repos/jesseduffield/lazydocker/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-  curl -sLo lazydocker.tar.gz "https://github.com/jesseduffield/lazydocker/releases/latest/download/lazydocker_${lazydocker_version}_Linux_x86_64.tar.gz"
-  tar -xf lazydocker.tar.gz lazydocker
-  sudo install lazydocker /usr/local/bin
-  rm lazydocker.tar.gz lazydocker
-  cd -
+  curl -sS https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | sh -s -- -y
 
   log_skipline
   log_success "Lazydocker installed"
